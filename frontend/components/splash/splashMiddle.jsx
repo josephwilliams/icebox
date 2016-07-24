f948043f4fb60acc4dd648a7bb18bd2eadd5c783
josephwilliams <josephwilli12@gmail.com>
2016-07-23 21:10:07 -0700

    splash page

--- frontend/components/splash/splashMiddle.jsx
+++ frontend/components/splash/splashMiddle.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
+var FontAwesome = require('react-fontawesome');
 
 var SplashMiddle = React.createClass({
 
   render: function() {
     return (
       <div className="splash-middle-container">
-        
+        <div className="splash-middle-object">
+          <div className="splash-middle-object-content">
+            <img src="images/beerglass.png"></img>
+            <h3>beers</h3>
+          </div>
+        </div>
+        <div className="splash-middle-object">
+          <div className="splash-middle-object-content">
+            <FontAwesome name="fa fa-flask fa-3x" />
+            <h3>breweries</h3>
+          </div>
+        </div>
       </div>
     );
   }
