fb37d8515a589688b3098e1f808312887381f15b
josephwilliams <josephwilli12@gmail.com>
2016-07-25 04:00:16 -0700

    shake hops

--- frontend/components/splash/splashFooter.jsx
+++ frontend/components/splash/splashFooter.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 
 const SplashFooter = React.createClass({
+  getInitialState: function () {
+      return({ hopsClass: "footer-top" });
+  },
+
+  componentDidMount: function() {
+      window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount: function() {
+      window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll: function(event) {
+      let scrollTop = event.srcElement.body.scrollTop;
+      this.setState({
+        hopsClass: "footer-top-wiggle"
+      });
+
+      this.handleScroll2();
+  },
+
+  handleScroll2: function (event) {
+    var that = this;
+    setTimeout(function () {
+      that.setState({
+        hopsClass: "footer-top"
+      });
+    }, 1000);
+
+    this.handleScroll();
+  },
+
   render: function() {
+    var classTitle = this.state.hopsClass;
     return (
-      <div className="splash-footer">
-        <div className="footer-left">
-          <p>about</p>
-          <p>beers</p>
-          <p>breweries</p>
-          <p>search</p>
-          <p>github</p>
+      <div className="footer">
+        <div className={classTitle} id="trigger">
+          <img src="images/hops.png" id="animate"></img>
         </div>
-        <div className="footer-right">
+        <div className="footer-bottom">
+            <div className="footer-bottom-left">
+              <p>about</p>
+              <p>beers</p>
+              <p>breweries</p>
+              <p>search</p>
+              <p>github</p>
+            </div>
+          <div className="footer-bottom-right">
+        </div>
         </div>
       </div>
     );
