f0fbc511605c771abae68d36707b0be20c4b0321
josephwilliams <josephwilli12@gmail.com>
2016-07-27 04:50:01 -0700

    form logic in place

--- frontend/components/shared/authModal/logInForm.jsx
+++ frontend/components/shared/authModal/logInForm.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
 
-class LogInForm extends React.Component {
-  componentDidMount () {
-    // if ($.browser.webkit) {
-    //   $('input[name="password"]').attr('autocomplete', 'off')
-    // }
-  }
-  
-  render () {
+var LogInForm = React.createClass ({
+  getInitialState: function () {
+    return ({ username: "", password: "" });
+  },
+
+  updateUsername: function (event) {
+    this.setState({ username: event.target.value });
+  },
+
+  updatePassword: function (event) {
+    this.setState({ password: event.target.value });
+  },
+
+  handleSubmit: function () {
+    var userData = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    $.ajax({
+      url: "api/session",
+      type: "POST",
+      data: {"user_params": {"username": userData.username, "password": userData.username}},
+      success: function (userData) {
+        console.log(userData)
+      }
+    });
+  },
+
+  render: function () {
     return (
       <div className="auth-form">
-        <form className="auth-form-inputs">
-          <input type="text" placeholder="username"></input>
-          <input type="password" placeholder="password"></input>
+        <form className="auth-form-inputs" onSubmit={this.handleSubmit}>
+          <input type="text"
+                 placeholder="username"
+                 onChange={this.updateUsername}
+                 value={this.state.username}></input>
+          <input type="password"
+                 placeholder="password"
+                 onChange={this.updatePassword}
+                 value={this.state.password}></input>
           <input type="submit" value="submit"></input>
           <img src="images/beer_row.jpg"></img>
         </form>
       </div>
     )
   }
-}
+});
 
 module.exports = LogInForm;
