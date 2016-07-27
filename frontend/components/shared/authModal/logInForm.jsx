1493a90bee344384bd3af3234814e3972bcd0250
josephwilliams <josephwilli12@gmail.com>
2016-07-27 02:59:48 -0700

    auth forms prepped

---
+++ frontend/components/shared/authModal/logInForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class LogInForm extends React.Component {
+  componentDidMount () {
+    // if ($.browser.webkit) {
+    //   $('input[name="password"]').attr('autocomplete', 'off')
+    // }
+  }
+  
+  render () {
+    return (
+      <div className="auth-form">
+        <form className="auth-form-inputs">
+          <input type="text" placeholder="username"></input>
+          <input type="password" placeholder="password"></input>
+          <input type="submit" value="submit"></input>
+          <img src="images/beer_row.jpg"></img>
+        </form>
+      </div>
+    )
+  }
+}
+
+module.exports = LogInForm;
