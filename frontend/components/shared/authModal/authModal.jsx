a15f0b21ab84233f3d8f4957db599d8c4f537e4f
davidnoah <david.t.noah@gmail.com>
2016-08-09 21:29:26 -0700

    fixed auth

--- frontend/components/shared/authModal/authModal.jsx
+++ frontend/components/shared/authModal/authModal.jsx
@@ -20,21 +20,21 @@ const AuthModal = React.createClass({
           <h4 className="auth-modal-header-unselected" onClick={this.toggleForm}>log in</h4>
           <h4 className="auth-modal-header-selected">sign up</h4>
         </div>
-      )
+      );
     } else {
       return (
         <div className="auth-modal-header">
           <h4 className="auth-modal-header-selected">log in</h4>
           <h4 className="auth-modal-header-unselected" onClick={this.toggleForm}>sign up</h4>
         </div>
-      )
+      );
     }
   },
 
   render: function() {
     var authForm = this.state.authType ?
       <SignUpForm toggleAuthModal={this.props.toggleAuthModal}/> :
-      <LogInForm toggleAuthModal={this.props.toggleAuthModal}/>
+      <LogInForm toggleAuthModal={this.props.toggleAuthModal}/>;
 
     return (
       <div className="auth-modal-container">
